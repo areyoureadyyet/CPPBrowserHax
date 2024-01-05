1f4c4c142458f17bb5d9f021a754dea96bb0f547
soumeswar <132776933+soumeswar@users.noreply.github.com>
2024-01-05 23:15:52 +0530

    Create BrowserHax.cpp

---
+++ BrowserHax.cpp
@@ -0,0 +1,43 @@
+#include <cstdlib>
+
+#include <iostream>
+
+
+using namespace std;
+
+
+int main() {
+
+    bool player_win;
+
+    string x;
+
+    cout << "Choose \"Windows\" or \"Linux\"" << endl;
+    cout << "User: ";
+    cin >> x;
+
+    if (x == "Windows" || "Linux") {
+        player_win = true;
+    } else {
+        player_win = false;
+        cout << "Bad Boy!";
+        cout << "Choose \"Windows\" or \"Linux\"";
+        cin >> x;
+    }
+    #ifdef _WIN32 
+    if (player_win == true) {
+        for (int i=0; i<=30; i++) {
+            int command = system("start https://www.blender.org");
+        }
+    } else {
+        return 0;
+    }
+    #elif __linux__
+    if (player_win == true) {
+        for (int i=0; i<=30; i++) {
+            int command = system("xdg-open https://www.blender.org");
+        }
+    }
+    #endif
+    return 0;
+}
